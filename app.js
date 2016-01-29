76baf95cac1b3b1e1129397a495e6ccf6836e34d
woest85 <woest85@hotmail.com>
2016-01-30 00:59:20 +0100

    Reverting failures

--- app.js
+++ app.js
@@ -17,20 +17,24 @@ var through = require('through');
 
 var app = express();
 
-//var google_analytics_id = process.env.GA_ID || null;
+var google_analytics_id = process.env.GA_ID || null;
 
 function addGa(html) {
-    //if (google_analytics_id) {
+    if (google_analytics_id) {
         var ga = [
-            "<script>"
-            "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');""
-                "ga('create', 'UA-73087864-1', 'auto');"
-                "ga('send', 'pageview');"
-                "ga('set', 'userId', {{USER_ID}});"
+            "<script type=\"text/javascript\">",
+            "var _gaq = []; // overwrite the existing one, if any",
+            "_gaq.push(['_setAccount', '" + google_analytics_id + "']);",
+            "_gaq.push(['_trackPageview']);",
+            "(function() {",
+            "  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;",
+            "  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';",
+            "  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);",
+            "})();",
             "</script>"
-            ].join("\n");
+        ].join("\n");
         html = html.replace("</body>", ga + "\n\n</body>");
-    //}
+    }
     return html;
 }
 
